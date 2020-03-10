99b464edd0b7f9ce5dfa5ea9b92ceae2f5bd7908
Fábio Vieira <43624576+eletricfabio@users.noreply.github.com>
2020-03-10 19:52:24 -0300

    DB_Aula1

---
+++ dbo.Aluno.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Aluno] (
+    [aln_Id_Aluno]      INT        IDENTITY (1, 1) NOT NULL,
+    [aln_Nome_Aluno]    NCHAR (50) NOT NULL,
+    [aln_Cpf_Aluno]     NCHAR (11) NULL,
+    [aln_Celular_Aluno] NCHAR (11) NULL,
+    [aln_Cidade_Aluno]  NCHAR (10) NULL,
+    PRIMARY KEY CLUSTERED ([aln_Id_Aluno] ASC)
+);
+
